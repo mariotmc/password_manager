bdd612d0c2391cb4798f44195c9a844dcf51eb4c
mariotmc <mariotomicx@gmail.com>
2024-12-08 21:38:23 +0100

    passwords shareable

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root "passwords#index"
 
   devise_for :users
-  resources :passwords
+  resources :passwords do
+    resources :shares, only: [:new, :create, :destroy]
+  end
 
   get "up" => "rails/health#show", as: :rails_health_check
 end
